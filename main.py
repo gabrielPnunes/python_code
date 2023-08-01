5a4a19dad8a77803f4c2e4358be48aaf92ff48c6
gabrielPnunes <95189995+gabrielPnunes@users.noreply.github.com>
2023-08-01 19:12:15 -0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,10 @@
+nome = input("qual o seu nome")
+print("É um prazer te conhecer,{}!".format(nome))
+
+#dia = input("qual o dia do seu nascimento?")
+#mes = input("qual o mês em que nasceu?")
+#ano = input("ano em que nasceu?")
+#print("Você nasceu no dia", dia , "do" , mes , "de", ano) 
+
+conta = ( 8 + 2)
+print(conta)
